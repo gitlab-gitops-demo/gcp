fe6605d1004d9e673558f432e3c13d8d37ccf3a5
Brad Downey <bdowney@gitlab.com>
2019-09-24 14:36:02 -0700

    Add GKE cluster

---
+++ gke.tf
@@ -0,0 +1,21 @@
+// Configure the Google Cloud resources
+resource "google_container_cluster" "primary" {
+  name               = "gitops-demo-gke"
+  location           = "us-west1-a"
+  initial_node_count = 2
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      Terraform = "true"
+    }
+
+    tags = ["Terraform", "gitops-demo"]
+  }
+}
