8c5f4654bbad68f63c2ef46c544462f95d0040f0
Brad Downey <bdowney@gitlab.com>
2019-10-02 15:12:58 -0700

    Change env scope of group cluster

--- group_cluster.tf
+++ group_cluster.tf
@@ -11,7 +11,7 @@ resource "gitlab_group_cluster" "gke_cluster" {
   group              = "${data.gitlab_group.gitops-demo-apps.id}"
   name               = "${google_container_cluster.primary.name}"
   domain             = "gke.gitops-demo.com"
-  environment_scope  = "*"
+  environment_scope  = "gcp/*"
   kubernetes_api_url = "https://${google_container_cluster.primary.endpoint}"
   kubernetes_token   = "${data.kubernetes_secret.gitlab-admin-token.data.token}"
   kubernetes_ca_cert = "${trimspace(base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate))}"
