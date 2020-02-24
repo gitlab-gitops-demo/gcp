6271f67e924fd2029fa5fdfe52f4aa701af3b9d9
Brad Downey <bdowney@gitlab.com>
2020-02-24 12:51:24 -0800

    Pin kerbernetes provider to 1.10.0 due to bug

--- gitlab-admin.tf
+++ gitlab-admin.tf
@@ -1,7 +1,7 @@
 data "google_client_config" "current" {}
 
 provider "kubernetes" {
-  version                = "~> 1.9"
+  version                = "1.10.0"
   host                   = google_container_cluster.primary.endpoint
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
   token                  = data.google_client_config.current.access_token
