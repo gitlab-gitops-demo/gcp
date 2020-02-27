8506321488ff5a5eec1f81ec4b2816446308d0de
Brad Downey <bdowney@gitlab.com>
2020-02-26 21:05:46 -0800

    Add workaround for management_project_id

--- group_cluster.tf
+++ group_cluster.tf
@@ -15,3 +15,20 @@ resource "gitlab_group_cluster" "gke_cluster" {
   kubernetes_ca_cert = trimspace(base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate))
 
 }
+
+# Work Around for lack of `management_project_id` in gitlab_group_cluster
+locals {
+  group_cluster_api_url = join("", ["https://gitlab.com/api/v4/", "groups/", gitlab_group_cluster.gke_cluster.group, "/clusters/", split(":", gitlab_group_cluster.gke_cluster.id)[1]])
+  curl_cmd = join("", ["curl -s --header \"Private-Token: $GITLAB_TOKEN\" ",
+    local.group_cluster_api_url,
+  " -H 'Content-Type:application/json' --request PUT --data '{\"management_project_id\":\"'$CLUSTER_MANAGEMENT_PROJECT_ID'\"}'"])
+}
+
+resource "null_resource" "gitlab-management-cluster-associate" {
+  triggers = { cluster_id = gitlab_group_cluster.gke_cluster.id }
+
+  provisioner "local-exec" {
+    command = local.curl_cmd
+  }
+  depends_on = [gitlab_group_cluster.gke_cluster]
+}
