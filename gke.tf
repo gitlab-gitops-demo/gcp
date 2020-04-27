9267f4827a235267dd0d839d831aa985043a174a
Brad Downey <bdowney@gitlab.com>
2020-04-27 17:09:33 +0000

    Reduce node count to 3.

--- gke.tf
+++ gke.tf
@@ -2,7 +2,7 @@
 resource "google_container_cluster" "primary" {
   name               = "gitops-demo-gke"
   location           = "us-west1-a"
-  initial_node_count = 7
+  initial_node_count = 3
 
   node_config {
     oauth_scopes = [
