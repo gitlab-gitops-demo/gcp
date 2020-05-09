71d1b138e58bc20f1ee86d7937a9f85f50a75481
Brad Downey <bdowney@gitlab.com>
2020-05-09 14:49:36 -0700

    Remove version constraint

--- gitlab-admin.tf
+++ gitlab-admin.tf
@@ -1,7 +1,6 @@
 data "google_client_config" "current" {}
 
 provider "kubernetes" {
-  version                = "1.10.0"
   host                   = google_container_cluster.primary.endpoint
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
   token                  = data.google_client_config.current.access_token
