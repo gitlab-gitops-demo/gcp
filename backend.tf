7189c66dbb3f54d8a9c38120fdbc96f3943bfdc4
Brad Downey <bdowney@gitlab.com>
2020-05-15 16:03:39 -0700

    Add config to backend.tf

--- backend.tf
+++ backend.tf
@@ -1,4 +1,11 @@
 terraform {
   backend "http" {
+    address        = "https://gitlab.com/api/v4/projects/14379215/terraform/state/gcp"
+    lock_address   = "https://gitlab.com/api/v4/projects/14379215/terraform/state/gcp/lock"
+    unlock_address = "https://gitlab.com/api/v4/projects/14379215/terraform/state/gcp/lock"
+    username       = "tf"
+    lock_method    = "POST"
+    unlock_method  = "DELETE"
+    retry_wait_min = "5"
   }
 }
