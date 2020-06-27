5aa586f84290b3b55369215756064e5f6b0fd553
Brad Downey <bdowney@gitlab.com>
2020-06-27 12:02:26 -0700

    Increase node size and move to node pool

--- gke.tf
+++ gke.tf
@@ -1,20 +1,30 @@
 // Configure the Google Cloud resources
 resource "google_container_cluster" "primary" {
-  name               = "gitops-demo-gke"
-  location           = "us-west1-a"
-  initial_node_count = 4
+  name                     = "gitops-demo-gke"
+  location                 = "us-west1-a"
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "us-west1-a"
+  cluster    = google_container_cluster.primary.name
+  node_count = 3
 
   node_config {
+    preemptible  = true
+    machine_type = "n2-standard-4"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
     oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
-
-    labels = {
-      Terraform = "true"
-    }
   }
 }
 
